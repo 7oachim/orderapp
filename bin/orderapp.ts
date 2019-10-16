105407795bc143a5953611b04b0bc96cdc23f2a3
Joachim Wiedmann <joachim.wiedmann@consol.de>
2019-10-16 16:41:19 +0200

    added other stacks

--- bin/orderapp.ts
+++ bin/orderapp.ts
@@ -2,7 +2,14 @@
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { OrderappStackJw } from '../lib/orderapp-stack';
+import { BackendStack } from '../lib/backend-stack';
+import { FrontendStack } from '../lib/frontend-stack';
 
 
 const app = new cdk.App();
-new OrderappStackJw(app, 'OrderappStackJw');
+const orderappStackJw = new OrderappStackJw(app, 'OrderappStackJw');
+const backendStack = new BackendStack(app, 'BackendStackJw');
+const frontendStack = new FrontendStack(app, 'FrontendStackJw');
+
+const ownerTag = new cdk.Tag("owner", "Max.Mustermann");
+[orderappStackJw, backendStack, frontendStack].forEach(stack => ownerTag.visit(stack));
