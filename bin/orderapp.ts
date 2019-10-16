e32164d911422714b4ed1082dee34a3f1b29e935
Joachim Wiedmann <joachim.wiedmann@consol.de>
2019-10-16 15:45:48 +0200

    added lamdba

--- bin/orderapp.ts
+++ bin/orderapp.ts
@@ -3,5 +3,6 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { OrderappStack } from '../lib/orderapp-stack';
 
+
 const app = new cdk.App();
 new OrderappStack(app, 'OrderappStack');
--- lib/orderapp-stack.ts
+++ lib/orderapp-stack.ts
@@ -1,9 +1,15 @@
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda')
+import apigw = require('@aws-cdk/aws-apigateway')
 
 export class OrderappStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const echo = new lambda.Function(this, "EchoFunction", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.asset('echo-handler'),
+      handler: "main.handler"
+    });
   }
 }
